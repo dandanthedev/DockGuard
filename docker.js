2495f13cfebcfe5228fb9ee945a7a7df06934d3d
Daan <20974756+daanschenkel@users.noreply.github.com>
2024-02-17 10:54:54 +0100

    Look for other docker sockets

--- docker.js
+++ docker.js
@@ -1,5 +1,18 @@
 const { Docker } = require("node-docker-api");
+const fs = require("fs");
+//find socketpath
+let socketPath;
+if (fs.existsSync("//./pipe/docker_engine")) {
+  socketPath = "//./pipe/docker_engine";
+} else if (fs.existsSync("/var/run/docker.sock")) {
+  socketPath = "/var/run/docker.sock";
+} else {
+  console.log(
+    "🦆 I couldn't find a docker socket. Please make sure docker is installed and running."
+  );
+  process.exit(1);
+}
 
-const docker = new Docker({ socketPath: "//./pipe/docker_engine" });
+const docker = new Docker({ socketPath });
 
 module.exports = docker;
